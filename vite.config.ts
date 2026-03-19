5600043f68fed34c7a417e5ed67daac639be0c14
trkbt10 <triplequartet@gmail.com>
2026-03-19 14:31:01 +0900

    Initial commit (via bun create)

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/**
+ * @file Vite build configuration
+ */
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts(),
+  ],
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: "src/index.ts",
+      formats: ["cjs", "es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: [/node:.+/],
+    },
+  },
+});
